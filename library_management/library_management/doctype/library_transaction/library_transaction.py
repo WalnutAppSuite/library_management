2516a93a98886cfb9d62ee2f6b8cb8ba75d6f3c8
saicharan <saicharan.kamble@unityedu.ai>
2025-12-22 03:52:48 -0500

    fix/indentation

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -1,11 +1,9 @@
 # Copyright (c) 2013, Frappe
 # For license information, please see license.txt
-
 from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-
 class LibraryTransaction(Document):
 	def validate(self):
 		last_transaction = frappe.get_list("Library Transaction",
