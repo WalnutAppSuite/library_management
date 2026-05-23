927702f03e6be53317df21b0ccc526b2aaa3a34b
ankitpatil-ap <ankitpatil.v@gmail.com>
2026-05-23 11:05:24 +0000

    fix(migration): heal 'NA' quantity rows before schema ALTER

--- library_management/library_management/doctype/library_books/library_books.py
+++ library_management/library_management/doctype/library_books/library_books.py
@@ -4,15 +4,13 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import get_link_to_form
+from frappe.utils import cint, get_link_to_form
 
 
 class LibraryBooks(Document):
 	def validate(self):
-		if not self.quantity:
-			self.quantity = 1
-		if self.available_quantity is None:
-			self.available_quantity = self.quantity
+		self.quantity = cint(self.quantity) or 1
+		self.available_quantity = cint(self.available_quantity) or self.quantity
 		if self.accession_number and self.branch:
 			existing = frappe.get_value(
 				"Library Books",
